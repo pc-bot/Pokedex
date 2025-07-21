84114e0201fd5d1e2c223e215bc4d5736b91d98c
antso <hei.antso.2007@gmail.com>
2025-07-21 07:24:21 +0300

    ajout de useffect et fetch sur le pokedex

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -4,7 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [pokemonList, setPokemonList] = useState([]);
+
+useEffect(() => {
+  fetch('https://pokeapi.co/api/v2/pokedex/1/')
+    .then((response) => response.json())
+    .then((result) => {
+      const pokemonPromises = result.pokemon_entries.map(entry => 
+        fetch(entry.pokemon_species.url.replace('-species', ''))
+          .then(res => res.json())
+      );
+      
+      Promise.all(pokemonPromises).then(pokemonDetails => {
+        const pokemonData = pokemonDetails.map(pokemon => ({
+          name: pokemon.name,
+          image: pokemon.sprites.front_default
+        }));
+        setPokemonList(pokemonData);
+      });
+    })
+    .catch((error) => console.error("Erreur :", error));
+}, []);
 
   return (
   <div className='font-bold bg-red-300'>hello world</div>
